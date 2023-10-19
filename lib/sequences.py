faff8a17dad2bf994d5ccfdb463f75920c5e71c4
Mark Mutugi <mutugimg@gmail.com>
2023-10-19 12:52:53 +0300

    Completed Assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    
+    if length == 0:
+        print([])
+        return
+    elif length == 1:
+        print ([0])
+        return
+        
+    
+    fib_list = [0,1]
+    while len(fib_list) < length:
+        next_value = fib_list[-1] + fib_list[-2]
+        fib_list.append(next_value)
+    print(fib_list)
+    
+    
+
+# print_fibonacci(10)
